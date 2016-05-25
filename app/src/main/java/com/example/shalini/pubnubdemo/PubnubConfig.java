545bbf8835e707f847a1ea770550cca9b5d4d507
Shalini Prajesh <shalini.prajesh@tothenew.com>
2016-05-25 11:01:51 +0530

    key changes

--- app/src/main/java/com/example/shalini/pubnubdemo/PubnubConfig.java
+++ app/src/main/java/com/example/shalini/pubnubdemo/PubnubConfig.java
@@ -4,8 +4,8 @@ package com.example.shalini.pubnubdemo;
  * Created by shalini on 14/5/16.
  */
 public class PubnubConfig {
-    public static final String SUBSCRIBER_KEY = "sub-c-876fe1f8-18ce-11e6-be83-0619f8945a4f";
-    public static final String PUBLISH_KEY = "pub-c-1989e0a8-39d0-42d1-931f-99cb4dcb77de";
+    public static final String SUBSCRIBER_KEY = "<Your subscriber key>";  // replace your publisher and subscriber key here
+    public static final String PUBLISH_KEY = "<Your publisher key>";
     public static final String CHANNEL_NAME = "Channel-56f7eh35y";
 
 
